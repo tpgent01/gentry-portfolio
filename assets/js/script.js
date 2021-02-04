b5c651b5754d421c3cfa9c95045be4331b32a6a8
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-03 20:14:31 -0600

    added icons to contact section

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-<script language="javascript" type="text/javascript">
-  $(document).ready(function() {
-    $('#trigger').click(function(){
-      $("#dialog").dialog();
-    }) 
-  });                  
-</script>
